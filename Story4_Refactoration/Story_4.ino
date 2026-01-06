c11a6e789d5cdaa79a61cc250f68aaf6cd51e46a
“vanshikapopat” <vanshika.popat@gmail.com>
2026-01-06 17:10:47 +0000

    Refactored Story 4: separate functions for each rover movement

--- Story4_Refactoration/Story_4.ino
+++ Story4_Refactoration/Story_4.ino
@@ -1,43 +1,68 @@
-const int RED_LED = 2;
-const int YELLOW_LED = 3;
-const int GREEN_LED_1 = 4;
-const int GREEN_LED_2 = 5;
+// Story 4: Refactored Rover Movements with Functions
 
+// Motor control pins
+int leftRev = 12;
+int leftFwd = 11;
+int leftEnable = 10;
+
+int rightEnable = 9;
+int rightFwd = 8;
+int rightRev = 7;
+
+// Setup
 void setup() {
-  pinMode(RED_LED, OUTPUT);
-  pinMode(YELLOW_LED, OUTPUT);
-  pinMode(GREEN_LED_1, OUTPUT);
-  pinMode(GREEN_LED_2, OUTPUT);
+  pinMode(leftRev, OUTPUT);
+  pinMode(leftFwd, OUTPUT);
+  pinMode(leftEnable, OUTPUT);
+
+  pinMode(rightEnable, OUTPUT);
+  pinMode(rightFwd, OUTPUT);
+  pinMode(rightRev, OUTPUT);
 }
 
+// Function: Move Forward
+void moveForward() {
+  digitalWrite(leftEnable, HIGH);
+  digitalWrite(leftFwd, HIGH);
+  digitalWrite(leftRev, LOW);
+
+  digitalWrite(rightEnable, HIGH);
+  digitalWrite(rightFwd, HIGH);
+  digitalWrite(rightRev, LOW);
+
+  delay(5000); // 5 seconds forward
+}
+
+// Function: Stop Rover
+void stopRover() {
+  digitalWrite(leftEnable, LOW);
+  digitalWrite(rightEnable, LOW);
+
+  digitalWrite(leftFwd, LOW);
+  digitalWrite(leftRev, LOW);
+  digitalWrite(rightFwd, LOW);
+  digitalWrite(rightRev, LOW);
+
+  delay(1000); // 1 second stop
+}
+
+// Function: Move Reverse
+void moveReverse() {
+  digitalWrite(leftEnable, HIGH);
+  digitalWrite(leftFwd, LOW);
+  digitalWrite(leftRev, HIGH);
+
+  digitalWrite(rightEnable, HIGH);
+  digitalWrite(rightFwd, LOW);
+  digitalWrite(rightRev, HIGH);
+
+  delay(2000); // 2 seconds reverse
+}
+
+// Main Loop
 void loop() {
-  moveForward(5000); // 5 seconds
-  stopRobot(1000);   // 1 second
-  moveReverse(2000); // 2 seconds
-}
-
-// Movement Functions
-
-void moveForward(int duration) {
-  digitalWrite(GREEN_LED_1, HIGH);
-  digitalWrite(GREEN_LED_2, HIGH);
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(YELLOW_LED, LOW);
-  delay(duration);
-}
-
-void stopRobot(int duration) {
-  digitalWrite(GREEN_LED_1, LOW);
-  digitalWrite(GREEN_LED_2, LOW);
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(YELLOW_LED, LOW);
-  delay(duration);
-}
-
-void moveReverse(int duration) {
-  digitalWrite(GREEN_LED_1, LOW);
-  digitalWrite(GREEN_LED_2, LOW);
-  digitalWrite(RED_LED, HIGH);
-  digitalWrite(YELLOW_LED, HIGH);
-  delay(duration);
+  moveForward();
+  stopRover();
+  moveReverse();
+  stopRover(); 
 }
