26895a6adadd841d20d1e7657610f2e42c1cac67
“vanshikapopat” <vanshika.popat@gmail.com>
2026-01-07 20:37:17 +0000

    Update Story 7 figure-of-8 code and wiring screenshot

--- Story7_Figureof8/Story_7.ino
+++ Story7_Figureof8/Story_7.ino
@@ -1,65 +1,105 @@
+// STORY 7 - SQUARE FIGURE OF 8 
 
-// Pins
-const int MOTOR_L_FWD = 5;  
-const int MOTOR_R_FWD = 6;  
-const int MOTOR_L_REV = 3;
-const int MOTOR_R_REV = 2;
+const int LEFT_REV     = 12;
+const int LEFT_FWD     = 11;
+const int LEFT_ENABLE  = 10; 
 
-// Timing constants from instructions
-const int EXIT_TIME = 2000;   // 
-const int TURN_TIME = 850;    // 
-const int DRIVE_TIME = 1500;  // 
+const int RIGHT_ENABLE = 9;  
+const int RIGHT_FWD    = 8;
+const int RIGHT_REV    = 7;
 
-void setup() {
-  pinMode(MOTOR_L_FWD, OUTPUT);
-  pinMode(MOTOR_R_FWD, OUTPUT);
-  pinMode(MOTOR_L_REV, OUTPUT);
-  pinMode(MOTOR_R_REV, OUTPUT);
+const unsigned long EXIT_TIME  = 2000;  // leave garage (2s)
+const unsigned long TURN_TIME  = 850;   // 90 degree turn
+const unsigned long DRIVE_TIME = 1500;  // length of each side (adjust)
 
-  runStory6(); // Start on reset
+const int SPEED_STRAIGHT = 180;
+const int SPEED_TURN     = 180;
+
+void stopRobot() {
+  analogWrite(LEFT_ENABLE, 0);
+  analogWrite(RIGHT_ENABLE, 0);
+
+  digitalWrite(LEFT_FWD, LOW);
+  digitalWrite(LEFT_REV, LOW);
+  digitalWrite(RIGHT_FWD, LOW);
+  digitalWrite(RIGHT_REV, LOW);
 }
 
-void loop() {}
+void driveForward(int speedPWM) {
+  digitalWrite(LEFT_FWD, HIGH);
+  digitalWrite(LEFT_REV, LOW);
+  digitalWrite(RIGHT_FWD, HIGH);
+  digitalWrite(RIGHT_REV, LOW);
 
-void runStory6() {
-  // 1. Exit the Garage
-  moveStraight(255, EXIT_TIME);
-  
-  // 2. Left Hand Square
-  for (int i = 0; i < 4; i++) {
-    moveStraight(255, DRIVE_TIME);
-    turnLeft(255, TURN_TIME);
-  }
+  analogWrite(LEFT_ENABLE, speedPWM);
+  analogWrite(RIGHT_ENABLE, speedPWM);
+}
 
-  // 3. Right Hand Square
+void driveForwardFor(unsigned long ms, int speedPWM) {
+  driveForward(speedPWM);
+  delay(ms);
+}
+
+void turnLeftFor(unsigned long ms, int speedPWM) {
+  digitalWrite(LEFT_FWD, HIGH);
+  digitalWrite(LEFT_REV, LOW);
+  digitalWrite(RIGHT_FWD, HIGH);
+  digitalWrite(RIGHT_REV, LOW);
+
+  analogWrite(LEFT_ENABLE, 0);       
+  analogWrite(RIGHT_ENABLE, speedPWM);
+  delay(ms);
+}
+
+void turnRightFor(unsigned long ms, int speedPWM) {
+  digitalWrite(LEFT_FWD, HIGH);
+  digitalWrite(LEFT_REV, LOW);
+  digitalWrite(RIGHT_FWD, HIGH);
+  digitalWrite(RIGHT_REV, LOW);
+
+  analogWrite(LEFT_ENABLE, speedPWM);
+  analogWrite(RIGHT_ENABLE, 0);      
+  delay(ms);
+}
+
+
+void leftHandSquare() {
   for (int i = 0; i < 4; i++) {
-    moveStraight(255, DRIVE_TIME);
-    turnRight(255, TURN_TIME);
+    driveForwardFor(DRIVE_TIME, SPEED_STRAIGHT);
+    turnLeftFor(TURN_TIME, SPEED_TURN);
   }
+}
+
+void rightHandSquare() {
+  for (int i = 0; i < 4; i++) {
+    driveForwardFor(DRIVE_TIME, SPEED_STRAIGHT);
+    turnRightFor(TURN_TIME, SPEED_TURN);
+  }
+}
+
+
+void story7_FigureOf8() {
+  driveForwardFor(EXIT_TIME, SPEED_STRAIGHT);
+  leftHandSquare();
+
+  rightHandSquare();
+
+  stopRobot();
+  while (true) { }
+}
+
+void setup() {
+  pinMode(LEFT_REV, OUTPUT);
+  pinMode(LEFT_FWD, OUTPUT);
+  pinMode(LEFT_ENABLE, OUTPUT);
+
+  pinMode(RIGHT_ENABLE, OUTPUT);
+  pinMode(RIGHT_FWD, OUTPUT);
+  pinMode(RIGHT_REV, OUTPUT);
 
   stopRobot();
 }
 
-
-void moveStraight(int speed, int duration) {
-  analogWrite(MOTOR_L_FWD, speed);
-  analogWrite(MOTOR_R_FWD, speed);
-  delay(duration);
-}
-
-void turnLeft(int speed, int duration) {
-  analogWrite(MOTOR_L_FWD, 0);     // Stop left
-  analogWrite(MOTOR_R_FWD, speed); // Drive right
-  delay(duration);
-}
-
-void turnRight(int speed, int duration) {
-  analogWrite(MOTOR_L_FWD, speed); // Drive left
-  analogWrite(MOTOR_R_FWD, 0);     // Stop right
-  delay(duration);
-}
-
-void stopRobot() {
-  digitalWrite(MOTOR_L_FWD, LOW);
-  digitalWrite(MOTOR_R_FWD, LOW);
+void loop() {
+  story7_FigureOf8();  
 }
